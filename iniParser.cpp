1f8bb8a659c9d7b0a8517b0811a7b04cf9489ac2
AOI <miho0_0@naver.com>
2022-07-04 18:13:39 +0900

    first upload iniParser files

---
+++ iniParser.cpp
@@ -0,0 +1,39 @@
+﻿#include "pch.h"
+
+CCriticalSection g_criticalExe;
+
+void iniwriteString(CString strAppName, CString strKeyName, CString strValue, CString strFilePath)
+{
+	WritePrivateProfileString(strAppName, strKeyName, strValue, strFilePath);
+}
+
+CString iniReadString(CString strAppName, CString strKeyName, CString strFilePath)
+{
+	CString szReturnString;
+	LPWSTR szReturnTmp = (LPWSTR)(LPCWSTR)szReturnString;
+
+
+	GetPrivateProfileString(strAppName, strKeyName, _T(""), szReturnTmp, 1024, strFilePath);
+
+	CString strTemp;
+	strTemp.Format(_T("%s"), szReturnTmp);
+
+	return strTemp;
+}
+
+
+CString GetExePath()
+{
+	g_criticalExe.Lock();
+	static TCHAR pBuf[256] = { 0, };
+	memset(pBuf, NULL, sizeof(pBuf));
+
+	GetModuleFileName(NULL, pBuf, sizeof(pBuf));
+
+	CString strFilePath;
+	strFilePath.Format(_T("%s"), pBuf);
+	strFilePath = strFilePath.Left(strFilePath.ReverseFind(_T('\\')));
+	g_criticalExe.Unlock();
+
+	return strFilePath;
+}
---
+++ iniParser.h
@@ -0,0 +1,8 @@
+﻿#ifndef __INI_PARSER__
+#define __INI_PARSER__
+
+void iniwriteString(CString strAppName, CString strKeyName, CString strValue, CString strFilePath);
+CString iniReadString(CString strAppName, CString strKeyName, CString strFilePath);
+CString GetExePath();
+
+#endif __INI_PARSER__
